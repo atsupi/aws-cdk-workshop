72afb247c03c592191d1769b59a4cc78a0208c51
atsupi <atsupi@gmail.com>
2023-03-25 00:09:26 +0900

    Added RemovalPolicy of table.

--- lib/hitconter.ts
+++ lib/hitconter.ts
@@ -17,11 +17,15 @@ export class HitCounter extends Construct {
     constructor(scope: Construct, id: string, props: HitCounterProps) {
         super(scope, id);
   
-        const table = new dynamodb.Table(this, 'Hits', {
-            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+        const table = new dynamodb.Table(this, "Hits", {
+            partitionKey: {
+              name: "path",
+              type: dynamodb.AttributeType.STRING
+            },
+            removalPolicy: cdk.RemovalPolicy.DESTROY
         });
         this.table = table;
-  
+
         this.handler = new lambda.Function(this, 'HitCounterHandler', {
             runtime: lambda.Runtime.NODEJS_14_X,
             handler: 'hitcounter.handler',
